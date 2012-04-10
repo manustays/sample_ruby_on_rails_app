66541f6fea5ceb2f66b02687e9d1470477ae4910
Kumar Abhishek <manustays@gmail.com>
2012-04-10 09:29:03 +0530

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
 	validates :name, presence: true, length: { maximum: 100 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-	validates :password, length: { minimum: 6, maximum: 50 }
+	validates :password, length: { minimum: 5 }
 	validates :password_confirmation, presence: true
 end
 
